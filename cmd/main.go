075a736e971ee61542b1089669d955289d44fdf6
egolboy <52846039+egolboy@users.noreply.github.com>
2024-10-19 13:55:36 +0800

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/flipped-aurora/ws/core/biz"
-	"github.com/flipped-aurora/ws/core/data"
+	"github.com/egolboy/ws/core/biz"
+	"github.com/egolboy/ws/core/data"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
